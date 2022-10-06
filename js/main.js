7b4af9a9f59ec6a2cee355406529afb2b25e0d79
Dave86dev <davidochando1986@gmail.com>
2022-10-06 16:04:10 +0200

    Objetos y JS vanilla final

---
+++ js/main.js
@@ -0,0 +1,105 @@
+
+
+//CREAMOS UN ARRAY...
+
+
+// let y1 = ["X","X","O"];
+// let y2 = ["X","O","X"];
+// let y3 = ["O","","O"];
+
+// let matriz = [y1,y2,y3];
+
+// for(let i = 0; i < matriz.length; i++){
+
+//     // console.log(matriz[i]);
+//     if(matriz[2][1] === ""){
+//         console.log("Aqui se podría marcar una casilla...");
+//     };
+
+// };
+
+////////////////////////////////////////////////////////////
+
+
+// let frutas = ["manzana","platano","melocoton","melon","albaricoque"];
+
+// let bebidas = ["agua","fanta","cerveza","vino","ron","cazalla"];
+
+// let bebidasFrutas = frutas.concat(bebidas);
+
+// console.log(bebidasFrutas);
+// console.log(frutas.length);
+
+// let juntos = frutas.concat(bebidas).join("*");
+
+// console.log(juntos);
+
+// let numeros = [5,6,1,2,77,75,98,76123];
+
+// numeros.sort();
+
+// console.log(numeros);
+
+// let videojuegos = ["lost ark","warframe","lol","wow","metal slug"];
+
+// let posibles = videojuegos.filter(juego => juego != "lol");
+
+// console.log(posibles);
+
+/////////////////////////////////////////////////////////
+
+// let num = parseInt(prompt("Introduce el número de dni"));
+
+// let letra = prompt("Introduce la letra del DNI");
+
+// let array = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E", "T"];
+
+// letra = letra.toUpperCase();
+
+// let pos = num % 23;
+
+// if (letra == array[pos]) {
+//     console.log("Es correcto")
+// } else {
+//     console.log("No es correecto")
+// };
+
+
+////////////////////////////////////////////////////////
+
+// let dniNumber = parseInt(prompt("Introduce el número del dni, sin letra"));
+
+// let modulo = dniNumber % 23;
+
+// let letter = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E", "T"];
+
+// let dniLetter = letter[modulo];
+
+// console.log("La letra es " + dniLetter);
+
+// let carlos = ["carlos",27,"Valencia","Videojuegos"];
+
+// let alumnoCarlos = {
+//     nombre: "carlos",
+//     edad: 27,
+//     ciudad: "Valencia",
+//     hobbie: ["videojuegos","peliculas","leer","viajar"],
+//     amigos : {
+
+//         amigo1 : "Jose",
+//         amigo2 : "Marta",
+//         amigo3 : {
+//             nombreAmigo3 : "Mara",
+//             hobbiesAmigo3 : {
+//                 hobbieMara1: "Leer",
+//                 hobbieMara2: "Pintar"
+//             }
+//         }
+
+//     }
+// };
+
+// console.log(alumnoCarlos.ciudad);
+// console.log(alumnoCarlos.amigos.amigo3);
+
+/////
